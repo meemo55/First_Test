2523991b904b425cc8c3e21ef916e32f4cfa82be
meera <meeramore29@gmail.com>
2017-01-10 22:00:14 +0530

    Initial Commit

---
+++ First_Test/src/com/my/First_Test/Original.java
@@ -0,0 +1,11 @@
+package com.my.First_Test;
+
+public class Original {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Second sec = new Second();
+		sec.Message("Jenkins");
+	}
+
+}
---
+++ First_Test/src/com/my/First_Test/Second.java
@@ -0,0 +1,10 @@
+package com.my.First_Test;
+
+public class Second {
+
+	public void Message(String name) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello" + " "+name);
+	}
+
+}
